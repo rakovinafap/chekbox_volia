88aa0de2ff3cb2d08d9c2c5334e52df73e664644
Dmitriy <rakovinafap@gmail.com>
2023-08-30 23:25:05 +0300

    add favicon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import MainComponent from './mainComponent/mainComponent';
 import Footer from './footerPage/footerPage';
+import Favicon from 'react-favicon';
 
 
 import './App.css';
 
 function App(props) {
-  document.title = "chekbox_Volia";
+  document.title = "Массовое создание чеков";
+  
 
   return (
     <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+       <Favicon  url="https://cdn.icon-icons.com/icons2/1499/PNG/512/emblemdefault_103452.png" />
       <MainComponent />
       <Footer />
     </div>
